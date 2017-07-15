818da23fd85f6e105e600fc652630aad6e3e2f87
ju dantas <juds@gmail.com>
2017-07-14 23:25:07 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,23 @@
+
+'use strict'
+
+const sprites = []
+const palavraSecreta = 'judantas'
+const quantidadeLetras = palavraSecreta.length
+let life = 5;
+
+sprites[0] = '\t+-------+\n\t|\t|\n\tO\t|\n\t\t|\n\t\t|\n\t\t|\n=================\n'
+sprites[1] = '\t+-------+\n\t|\t|\n\tO\t|\n\t|\t|\n\t\t|\n\t\t|\n=================\n'
+sprites[2] = '\t+-------+\n\t|\t|\n\tO\t|\n\t|\\\t|\n\t\t|\n\t\t|\n=================\n'
+sprites[3] = '\t+-------+\n\t|\t|\n\tO\t|\n       /|\\\t|\n\t\t|\n\t\t|\n=================\n'
+sprites[4] = '\t+-------+\n\t|\t|\n\tO\t|\n       /|\\\t|\n       /\t|\n\t\t|\n=================\n'
+sprites[5] = '\t+-------+\n\t|\t|\n\tO\t|\n       /|\\\t|\n       / \\\t|\n\t\t|\n=================\n'
+
+/*process.stdin.on('data', function(input) {
+	input = String(input)
+	console.log(input)
+})*/
+
+const verificarLetraPalavra = (letra, palavra) => {
+
+}
